c8281a402e730d08fcad175cf898f0b3821e6f5a
User27 <arseniy.pro2709@gmail.com>
2024-12-08 12:31:09 +0200

    Added grade feature

--- 1.py
+++ 1.py
@@ -1,3 +1,22 @@
-print("hello world")
+grade = int(input("Яку оцінку ти отримав? "))
+if grade < 49:
+    print("Незадовільно")
+elif 50 <= grade <=69:
+    print("Задовільно")
+elif 70<= grade <= 89:
+    print("Добре")
+elif 90<= grade <= 100:
+    print("Відмінно")
 
-name=input("What`s your name?")
+a = int(input("Напиши перше число: "))
+b = int(input("Напиши друге число: "))
+operation = input("Напиши математичну дію: ")
+if operation == '+':
+    print(a+b)
+elif operation == '-':
+    print(a-b)
+elif operation == '*':
+    print(a*b)
+elif operation == '/':
+    if b == 0:
+        print("Помилка: ділення на нуль.")
